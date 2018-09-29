f0e3aa33ac593bb19066e0105955b3afbbda7b48
tad19 <tad19@truonganhdung>
2018-09-29 16:02:17 +0700

    Commit template.

---
+++ src/selenium_api/Templates.java
@@ -0,0 +1,37 @@
+package selenium_api;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Templates {
+    WebDriver driver;
+
+	@BeforeClass
+	public void beforeClass() {
+		driver = new FirefoxDriver();
+		//driver.get("http://live.guru99.com");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void TC_() {
+		driver.get("");
+		
+	}
+
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
