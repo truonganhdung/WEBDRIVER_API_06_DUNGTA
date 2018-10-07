de21a8faebe7c3f9912240ebfea7d7c32a3f6341
tad19 <tad19@truonganhdung>
2018-10-07 11:45:37 +0700

    Update Topic 05

--- src/selenium_api/Topic_05_HandleDropdown_CustomDropdown.java
+++ src/selenium_api/Topic_05_HandleDropdown_CustomDropdown.java
@@ -71,7 +71,7 @@ public class Topic_05_HandleDropdown_CustomDropdown {
 	 	*/
 		
 		//jqueryui
-		vefDrop("//span[@id='salutation-button']", "//ul[@id='salutation-menu']/li[@class='ui-menu-item']", "Other");
+		sltDrop("//span[@id='salutation-button']", "//ul[@id='salutation-menu']/li[@class='ui-menu-item']", "Other");
 		Assert.assertTrue(driver.findElement(By.xpath("//span[@id='salutation-button']//span[@class='ui-selectmenu-text' and text()='Other']")).isDisplayed());
 	}
 
@@ -80,7 +80,7 @@ public class Topic_05_HandleDropdown_CustomDropdown {
 		driver.get("https://material.angular.io/components/select/examples");
 
 		//angularjs
-		vefDrop("//mat-select[@placeholder='State']", "//mat-option//span", "Nevada");
+		sltDrop("//mat-select[@placeholder='State']", "//mat-option//span", "Nevada");
 		Assert.assertTrue(driver.findElement(By.xpath("//mat-select[@placeholder='State']//span[text()='Nevada']")).isDisplayed());
 	}
 
@@ -89,19 +89,27 @@ public class Topic_05_HandleDropdown_CustomDropdown {
 		driver.get("https://demos.telerik.com/kendo-ui/dropdownlist/index");
 		
 		//kendoui
-		vefDrop("//input[@id='color']/preceding-sibling::span", "//ul[@id='color_listbox']/li", "Grey");
+		sltDrop("//input[@id='color']/preceding-sibling::span", "//ul[@id='color_listbox']/li", "Grey");
 		Assert.assertTrue(driver.findElement(By.xpath("//span[text()='Grey']")).isDisplayed());
 	}
 	
-	@Test(enabled = true)
+	@Test(enabled = false)
 	public void TC_05_Handle_Vue() throws InterruptedException {
 		driver.get("https://mikerodham.github.io/vue-dropdowns/");
 
 		//vue-dropdowns
-		vefDrop("//li[@class='dropdown-toggle']", "//ul[@class='dropdown-menu']/li/a", "Third Option");
-		Assert.assertTrue(driver.findElement(By.xpath("//ul[@class='dropdown-menu hide']//a[contains(text(),'Third')]")).isDisplayed());
+		sltDrop("//li[@class='dropdown-toggle']", "//ul[@class='dropdown-menu']/li/a", "Third Option");
+		Assert.assertTrue(driver.findElement(By.xpath("//li[@class='dropdown-toggle' and contains(text(),'Third')]")).isDisplayed());
 	}
 	
+	@Test(enabled = true)
+	public void TC_06_Jquery_Editable_Select() throws InterruptedException {
+		driver.get("http://indrimuska.github.io/jquery-editable-select/");
+
+		//Jquery editable select
+		edtSltDrop("//div[@id='default-place']/input", "//div[@id='default-place']//li", "a", "Audi");
+		//Assert.assertTrue(driver.findElement(By.xpath("//li[@class='dropdown-toggle' and contains(text(),'Third')]")).isDisplayed());
+	}
 	
 	//( ._.')----------------------------------------------------
 	@AfterClass
@@ -111,9 +119,41 @@ public class Topic_05_HandleDropdown_CustomDropdown {
 	
 	
 	//( ._.')----------------------------------------------------
-	public void vefDrop(String parentLocator, String allItemsLocator, String expectedValue) throws InterruptedException  {
+	public void sltDrop(String parentLocator, String allItemsLocator, String expectedValue) throws InterruptedException  {
 		// 1 - Scroll to drop-down list then Click **parent locator** to expand all drop-down list
 		WebElement parantDropDown = driver.findElement(By.xpath(parentLocator));
+		((JavascriptExecutor) driver).executeScript("arguments[0].click();", parantDropDown);
+		//wait.until(ExpectedConditions.elementToBeClickable(parantDropDown));
+		Thread.sleep(1000);
+		
+		// 2 - get all items in list that have the same tag-name
+		List <WebElement> allItemsDropDown = driver.findElements(By.xpath(allItemsLocator));
+		int itemNumber = allItemsDropDown.size();
+		
+		wait.until(ExpectedConditions.visibilityOfAllElements(allItemsDropDown));
+		
+		// 3 - get text of all items
+		// 4 - compare w' text that input
+		// 5 - click if meet condition
+		for (int i = 0; i < itemNumber; i++) {
+			if (allItemsDropDown.get(i).getText().trim().equals(expectedValue)) {
+				((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", allItemsDropDown.get(i));
+				Thread.sleep(1000);
+				
+				allItemsDropDown.get(i).click();
+				break;
+			}
+		}
+		Thread.sleep(1000);
+	}
+	
+	//( ._.')----------------------------------------------------
+	public void edtSltDrop(String parentLocator, String allItemsLocator, String search, String expectedValue) throws InterruptedException  {
+		// 1 - Scroll to drop-down list then Click **parent locator** to expand all drop-down list
+		WebElement parantDropDown = driver.findElement(By.xpath(parentLocator));
+		parantDropDown.clear();
+		parantDropDown.sendKeys(search);
+		
 		((JavascriptExecutor) driver).executeScript("arguments[0].click();", parantDropDown);
 		//wait.until(ExpectedConditions.elementToBeClickable(parantDropDown));
 		Thread.sleep(1000);
