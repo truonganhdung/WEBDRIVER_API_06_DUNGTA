206de61d89ae9f329e19bc926de8693e2c65cf02
AzureAD\DungTruong <dung.truong@acomsolutions.com>
2018-10-17 13:51:31 +0700

    Update topic 05

--- src/selenium_api/Topic_05_HandleDropdown_CustomDropdown.java
+++ src/selenium_api/Topic_05_HandleDropdown_CustomDropdown.java
@@ -1,6 +1,6 @@
 /* Author: truonganhdung
  * Created Date: 10/05/2018
- * Modified Date: 10/08/2018
+ * Modified Date: 10/17/2018
  * */
 
 package selenium_api;
@@ -24,11 +24,14 @@ import org.testng.annotations.Test;
 public class Topic_05_HandleDropdown_CustomDropdown {
 	WebDriver driver;
 	WebDriverWait wait;
+	JavascriptExecutor js;
 	
 	@BeforeClass
 	public void beforeClass() {
 		driver = new FirefoxDriver();
 		wait = new WebDriverWait(driver, 30);
+		
+		js = (JavascriptExecutor) driver;
 		
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
@@ -54,9 +57,7 @@ public class Topic_05_HandleDropdown_CustomDropdown {
 	}
 	
 	@Test(enabled=false)
-	public void TC_02_Handle_JqueryDropdownList() throws Exception {
-		driver.get("http://jqueryui.com/resources/demos/selectmenu/default.html");
-		
+	public void TC_02() throws Exception {
 		/*
 		driver.findElement(By.xpath("//span[@id='salutation-button']")).click();
 		List <WebElement> list = driver.findElements(By.xpath("//ul[@id='salutation-menu']/li[@class='ui-menu-item']"));
@@ -70,54 +71,40 @@ public class Topic_05_HandleDropdown_CustomDropdown {
 		Assert.assertTrue(driver.findElement(By.xpath("//span[@id='salutation-button']//span[@class='ui-selectmenu-text' and text()='" + expectedItem + "']")).isDisplayed());
 	 	*/
 		
-		//jqueryui
+		//Handle_JqueryUI
+		driver.get("http://jqueryui.com/resources/demos/selectmenu/default.html");
+		
 		sltDrop("//span[@id='salutation-button']", "//ul[@id='salutation-menu']/li[@class='ui-menu-item']", "Other");
 		Assert.assertTrue(driver.findElement(By.xpath("//span[@id='salutation-button']//span[@class='ui-selectmenu-text' and text()='Other']")).isDisplayed());
-	}
-
-	@Test(enabled = false)
-	public void TC_03_Handle_AngularDropdownList() throws Exception {
+	
+		
+		//Handle_Angular
 		driver.get("https://material.angular.io/components/select/examples");
 
-		//angularjs
 		sltDrop("//mat-select[@placeholder='State']", "//mat-option//span", "Nevada");
 		Assert.assertTrue(driver.findElement(By.xpath("//mat-select[@placeholder='State']//span[text()='Nevada']")).isDisplayed());
-	}
-
-	@Test(enabled = false)
-	public void TC_04_Handle_Kendo() throws Exception {
+	
+		
+		//Handle_KendoUI
 		driver.get("https://demos.telerik.com/kendo-ui/dropdownlist/index");
 		
-		//kendoui
 		sltDrop("//input[@id='color']/preceding-sibling::span", "//ul[@id='color_listbox']/li", "Grey");
 		Assert.assertTrue(driver.findElement(By.xpath("//span[text()='Grey']")).isDisplayed());
-	}
 	
-	@Test(enabled = false)
-	public void TC_05_Handle_Vue() throws Exception {
+		
+		//Handle_Vue
 		driver.get("https://mikerodham.github.io/vue-dropdowns/");
 
-		//vue-dropdowns
 		sltDrop("//li[@class='dropdown-toggle']", "//ul[@class='dropdown-menu']/li/a", "Third Option");
 		Assert.assertTrue(driver.findElement(By.xpath("//li[@class='dropdown-toggle' and contains(text(),'Third')]")).isDisplayed());
 	}
 	
-	@Test(enabled = false)
-	public void TC_06_Jquery_Editable_Select() throws Exception {
-		driver.get("http://indrimuska.github.io/jquery-editable-select");
+	@Test(enabled = true)
+	public void TC_03_Jquery_Editable_Select() throws Exception {
+		driver.get("http://indrimuska.github.io/jquery-editable-select/");
 		
-		//Jquery editable select
-		edtSltDrop("//div[@id='default-place']/input", "//div[@id='default-place']//li", "a", "Audi");
-		//Assert.assertTrue(driver.findElement(By.xpath("//li[@class='dropdown-toggle' and contains(text(),'Third')]")).isDisplayed());
-	}
-	
-	@Test(enabled = false)
-	public void TC_07_Multiple_Select() throws Exception {
-		driver.get("http://wenzhixin.net.cn/p/multiple-select/docs");
-
-		//Jquery editable select
-		edtSltDrop("//div[@id='default-place']/input", "//div[@id='default-place']//li", "a", "Audi");
-		//Assert.assertTrue(driver.findElement(By.xpath("//li[@class='dropdown-toggle' and contains(text(),'Third')]")).isDisplayed());
+		edtDropDown("//div[@id='default-place']/input","//div[@id='default-place']//li","m", "BMW");
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@id='default-place']//li[text()='" + "BMW" + "']")).isDisplayed());
 	}
 	
 	//( ._.')----------------------------------------------------
@@ -155,34 +142,31 @@ public class Topic_05_HandleDropdown_CustomDropdown {
 		Thread.sleep(1000);
 	}
 	
-	public void edtSltDrop(String parentLocator, String allItemsLocator, String search, String expectedValue) throws Exception  {
-		// 1 - Scroll to drop-down list then Click **parent locator** to expand all drop-down list
-		WebElement parantDropDown = driver.findElement(By.xpath(parentLocator));
-		parantDropDown.clear();
-		parantDropDown.sendKeys(search);
+	public void edtDropDown(String parentLocator, String allItems, String search, String expectedValue) throws Exception{
+		WebElement parent = driver.findElement(By.xpath(parentLocator));
+		js.executeScript("arguments[0].scrollIntoView(true);",parent);
 		
-		((JavascriptExecutor) driver).executeScript("arguments[0].click();", parantDropDown);
-		//wait.until(ExpectedConditions.elementToBeClickable(parantDropDown));
+		parent.sendKeys(search);
 		Thread.sleep(1000);
+		List <WebElement> allItemsDropDown = driver.findElements(By.xpath(allItems));
 		
-		// 2 - get all items in list that have the same tag-name
-		List <WebElement> allItemsDropDown = driver.findElements(By.xpath(allItemsLocator));
-		int itemNumber = allItemsDropDown.size();
-		
-		wait.until(ExpectedConditions.visibilityOfAllElements(allItemsDropDown));
-		
-		// 3 - get text of all items
-		// 4 - compare w' text that input
-		// 5 - click if meet condition
-		for (int i = 0; i < itemNumber; i++) {
-			if (allItemsDropDown.get(i).getText().trim().equals(expectedValue)) {
-				((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", allItemsDropDown.get(i));
-				Thread.sleep(1000);
-				
+		int numberItem = allItemsDropDown.size(); 
+
+		for(int i=0;i<numberItem;i++)
+		{
+			if(allItemsDropDown.get(i).getText().equals(expectedValue))
+			{
+				((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);",allItemsDropDown.get(i) );
 				allItemsDropDown.get(i).click();
 				break;
 			}
 		}
 		Thread.sleep(1000);
+		
+		js.executeScript("window.scrollTo(0,-document.body.scrollHeight);");
+		Thread.sleep(1000);
+		
+		js.executeScript("arguments[0].scrollIntoView(true);",parent);
+		parent.click();
 	}
 }
