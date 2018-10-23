a218ac053503c00421854365107839eca0cc47e1
AzureAD\DungTruong <dung.truong@acomsolutions.com>
2018-10-23 15:24:12 +0700

    Commit POM Example from guru

---
+++ src/POMExample_SimplePOM/Guru99HomePage_SimplePOM.java
@@ -0,0 +1,19 @@
+package POMExample_SimplePOM;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Guru99HomePage_SimplePOM {
+
+	WebDriver driver;
+	By homePageUserName = By.xpath("//table//tr[@class='heading3']");
+	
+	public Guru99HomePage_SimplePOM(WebDriver driver){
+		this.driver = driver;
+	}
+	
+	//Get the User name from Home Page
+	public String getHomePageDashboardUserName(){
+		 return	driver.findElement(homePageUserName).getText();
+	}
+}
