88ea38bec3888f0487ec953542d2c1b14640093f
AzureAD\DungTruong <dung.truong@acomsolutions.com>
2018-11-05 11:18:14 +0700

    Finish Topic 13.

--- src/selenium_api/Topic_13_Wait.java
+++ src/selenium_api/Topic_13_Wait.java
@@ -1,18 +1,16 @@
 /* Author: truonganhdung
- * Created Date: xx/xx/2018
- * Modified Date: xx/xx/2018
+ * Created Date: 11/04/2018
+ * Modified Date: 11/05/2018
  * */
 
 package selenium_api;
 
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -20,10 +18,13 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import com.google.common.base.Function;
+
 
 public class Topic_13_Wait {
     WebDriver driver;
     WebDriverWait waitExplicit;
+    By selectedDate = By.xpath("//span[@id='ctl00_ContentPlaceholder1_Label1']");
 
     //( ._.')----------------------------------------------------
 	@BeforeClass
@@ -31,6 +32,8 @@ public class Topic_13_Wait {
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
+		
+		waitExplicit = new WebDriverWait(driver, 30);
 	}
 
 	//( ._.')----------------------------------------------------
@@ -46,20 +49,26 @@ public class Topic_13_Wait {
 	public void TC_02_ExplicitWait() {
 		driver.get("http://demos.telerik.com/aspnet-ajax/ajaxloadingpanel/functionality/explicit-show-hide/defaultcs.aspx");
 		
-		By dataPicker = By.xpath("//div[@class='rcMain']");
-		waitExplicit.until(ExpectedConditions.visibilityOfElementLocated(dataPicker));
+		//Check Date Picker display
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@id='ctl00_ContentPlaceholder1_Panel1']")).isDisplayed());
 		
-		WebElement selectedDate = driver.findElement(By.xpath("//span[@id='ctl00_ContentPlaceholder1_Label1']"));
+		//Verify selected date = "No Selected Dates to display."
+		WebElement selectedDateBefore = driver.findElement(selectedDate);
+		Assert.assertEquals(selectedDateBefore.getText(), "No Selected Dates to display.");
 		
-		System.out.println(selectedDate);
-//		Assert.assertEquals(selectedDate, "No Selected Dates to display.");
+		//pick 1 random day
+		driver.findElement(By.xpath("//a[text()='16']")).click();
 		
-//		driver.findElement(By.xpath("//a[text()='16']")).click();
-//		
-//		By ajaxIcon = By.xpath("//div[@class='raDiv']");
-//		waitExplicit.until(ExpectedConditions.invisibilityOfElementLocated(ajaxIcon));
-//		
-//		Assert.assertEquals(driver.findElement(selectedDate), "Friday, November 16, 2018");
+		//skip ajax loading
+		By ajaxIcon = By.xpath("//div[@class='raDiv']");
+		waitExplicit.until(ExpectedConditions.invisibilityOfElementLocated(ajaxIcon));
+		
+		//Verify selected date = 16 in Date Picker
+		Assert.assertTrue(driver.findElement(By.xpath("//td[@class='rcSelected']/a[text()='16']")).isDisplayed());
+		
+		//Verify selected date = "Friday, November 16, 2018"
+		WebElement selectedDateAfter = driver.findElement(selectedDate);
+		Assert.assertEquals(selectedDateAfter.getText(), "Friday, November 16, 2018");
 	}
 	
 	@Test(enabled=true)
@@ -67,6 +76,7 @@ public class Topic_13_Wait {
 		driver.get("https://daominhdam.github.io/fluent-wait/");
 		
 		WebElement countdount =  driver.findElement(By.xpath("//div[@id='javascript_countdown_time']"));
+		//Assert.assertTrue(countdount.isDisplayed());
 		waitExplicit.until(ExpectedConditions.visibilityOf(countdount));
 
 		new FluentWait<WebElement>(countdount)
@@ -75,10 +85,10 @@ public class Topic_13_Wait {
 		.ignoring(NoSuchElementException.class)
 		.until(new Function<WebElement, Boolean>() {
 			public Boolean apply(WebElement element) {
-			   boolean flag =  element.getText().endsWith("00");
-			   System.out.println("Time = " +  element.getText());
-			   return flag;
-			   }
+				boolean flag =  element.getText().endsWith("00");
+				System.out.println("Time = " +  element.getText());
+				return flag;
+			}
 		});
 	}
 
